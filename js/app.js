d0d67233c6c8d649698282e6afdd0fe78b7ffc4d
Krystian Jagoda <kristian2x@gmail.com>
2017-03-04 19:17:36 +0100

    Drop down menus

--- js/app.js
+++ js/app.js
@@ -1,79 +1,130 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     // submenu in HEADER
-    var li = document.querySelector('.main_menu').children[0]; //li : about us
-    var submenu = document.querySelector('.submenu'); //submenu
+    (function() {
 
-    li.addEventListener('mouseover', function(e) { //add event handler to change visibility from hidden to visible
-        submenu.style.visibility = 'visible';
+        var li = document.querySelector('.main_menu').children[0]; //li : about us
+        var submenu = document.querySelector('.submenu'); //submenu
+
+        li.addEventListener('mouseover', function(e) { //add event handler to change visibility from hidden to visible
+            submenu.style.visibility = 'visible';
 
 
-    });
-    li.addEventListener('mouseout', function(e) { //add event handler to change visibility from visible to hidden
-        submenu.style.visibility = 'hidden';
+        });
+        li.addEventListener('mouseout', function(e) { //add event handler to change visibility from visible to hidden
+            submenu.style.visibility = 'hidden';
 
-    });
+        });
+    })();
+
+
 
     // changing visibiliy of picture captions in s. 2
+    (function() {
 
-    var smallBoxes = document.querySelectorAll('.small_box');
+        var smallBoxes = document.querySelectorAll('.small_box');
 
+        for (var i = 0; i < 2; i++) {
 
+            smallBoxes[i].addEventListener('mouseover', function(e) {
+                var div = this.children[0];
+                div.style.display = 'none';
+                this.style.boxShadow = '0px 0px 10px 0px rgba(36,33,36,1)';
 
-    for (var i = 0; i < 2; i++) {
+            });
+        }
+        for (i = 0; i < 2; i++) {
 
-        smallBoxes[i].addEventListener('mouseover', function(e) {
-            var div = this.children[0];
-            div.style.display = 'none';
+            smallBoxes[i].addEventListener('mouseout', function(e) {
+                var div = this.children[0];
+                div.style.display = 'block';
+                this.style.boxShadow = '';
+
+            });
+        }
+        //last box, only box-shadow styling
+        smallBoxes[2].addEventListener('mouseover', function(e) {
             this.style.boxShadow = '0px 0px 10px 0px rgba(36,33,36,1)';
 
         });
-    }
-    for (i = 0; i < 2; i++) {
-
-        smallBoxes[i].addEventListener('mouseout', function(e) {
-            var div = this.children[0];
-            div.style.display = 'block';
+        smallBoxes[2].addEventListener('mouseout', function(e) {
             this.style.boxShadow = '';
 
         });
-    }
-    //last box, only box-shadow styling
-    smallBoxes[2].addEventListener('mouseover', function(e) {
-        this.style.boxShadow = '0px 0px 10px 0px rgba(36,33,36,1)';
+    })();
 
-    });
-    smallBoxes[2].addEventListener('mouseout', function(e) {
-        this.style.boxShadow = '';
 
-    });
 
     //Slider
-    var slider = document.querySelector('.slider');
+    (function() {
+        var slider = document.querySelector('.slider');
 
-    var arrow1 = document.querySelector('.arrow1');
-    var arrow2 = document.querySelector('.arrow2');
+        var arrow1 = document.querySelector('.arrow1');
+        var arrow2 = document.querySelector('.arrow2');
 
-    var lis = slider.querySelectorAll('ul>li');
-    var index = 0;
-    lis[index].classList.add('visible');
-
-
-    arrow2.addEventListener('click', function() {
-        lis[index].classList.remove('visible');
-        index++;
-        if (index > lis.length - 1) {
-            index = 0;
-        }
+        var lis = slider.querySelectorAll('ul>li');
+        var index = 0;
         lis[index].classList.add('visible');
-    });
 
-    arrow1.addEventListener('click', function() {
-        lis[index].classList.remove('visible');
-        index--;
-        if (index < 0) {
-            index = lis.length - 1;
+
+        arrow2.addEventListener('click', function() {
+            lis[index].classList.remove('visible');
+            index++;
+            if (index > lis.length - 1) {
+                index = 0;
+            }
+            lis[index].classList.add('visible');
+        });
+
+        arrow1.addEventListener('click', function() {
+            lis[index].classList.remove('visible');
+            index--;
+            if (index < 0) {
+                index = lis.length - 1;
+            }
+            lis[index].classList.add('visible');
+        });
+    })();
+
+
+    // Drop down menus - selecting elements of a chair
+    (function() {
+
+        var arrows = document.querySelectorAll('.list_arrow');
+
+        for (var i = 0; i < arrows.length; i++) {
+            arrows[i].addEventListener('click', function(e) {
+
+                var ul = this.nextElementSibling;
+                var listEls = ul.querySelectorAll('li');
+
+
+                ul.style.display = 'block';
+
+                for (var i = 0; i < listEls.length; i++) {
+
+                    listEls[i].addEventListener('click', function(e) {
+                        var span = this.parentElement.parentElement.firstElementChild;
+                        span.innerText = this.innerText;
+                        this.parentElement.style.display = 'none';
+                    });
+                }
+
+
+            });
+
         }
-        lis[index].classList.add('visible');
-    });
+
+
+
+    })();
+
+
+
+
+
+
+
+
+
 });
--- readme.md
+++ readme.md
@@ -1,7 +1,21 @@
 Sit-On-Chair
 
 Please look at my first website prepared on bootcamp in [Coders' Lab.](https://github.com/CodersLab)
+Sit-on-Chair
 
+smaller PSD project presenting basic page functionality
+
+Coded using:
+
+HTML5
+CSS
+JavaScript
+Features:
+
+Dropdown menus
+Simple image slider
+Hoverable elements
+Calculator
 
 Sit on chair is a simple website project based on a PSD file.
 
